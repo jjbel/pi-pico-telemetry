95c06882fa9eb685d4d945b6fc2abbc5be39f637
Jai Bellare <jaibellare@gmail.com>
2025-02-14 22:52:48 +0530

    flask webserver to serve index.html

---
+++ README.md
@@ -0,0 +1,5 @@
+## Pi-Pico communication
+
+[Geekculture Medium](https://medium.com/geekculture/serial-connection-between-raspberry-pi-and-raspberry-pico-d6c0ba97c7dc)
+
+[How to enable Tx Rx pins on Pi](https://spellfoundry.com/2016/05/29/configuring-gpio-serial-port-raspbian-jessie-including-pi-3-4/)
---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from flask import Flask, redirect
+from pathlib import Path
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='./')
+
+# index.html isn't a template, so just serve it as a static file
+# TODO find better way to this
+# flask shd be run with an actual http server like Apache
+# https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+
+# doesn't redirect / to index.html by default
+
+@app.route('/')
+def index():
+    return redirect('/index.html')
+
+if __name__ == '__main__':
+   app.run()
